a75614df82c8b8b087ce59a3a372e30cbf3b364a
Neetan <75791553+nplasar@users.noreply.github.com>
2021-04-27 13:40:39 +0100

    Create waiter_start_instance.py

---
+++ waiter_start_instance.py
@@ -0,0 +1,20 @@
+import boto3
+
+aws_mag_con = boto3.session.Session(profile_name="XXXX")
+ec2_con_res = aws_mag_con.resource(service_name="ec2",region_name="eu-west-2")
+ec2_con_cli = aws_mag_con.client(service_name="ec2",region_name="eu-west-2")
+
+#Using Resource
+#ec2_ins_obj = ec2_con_res.Instance("i-0cff5fb2a61c96e5a")
+#print("Instance is starting")
+#ec2_ins_obj.start()
+#print(dir(ec2_ins_obj))
+#ec2_ins_obj.wait_until_running()
+##print("Instance is now running")
+
+#Using Waiter
+print("Instance is starting")
+ec2_con_cli.start_instances(InstanceIds=['i-0cff5fb2a61c96e5a'])
+waiter=ec2_con_cli.get_waiter('instance_running')
+waiter.wait(InstanceIds=['i-0cff5fb2a61c96e5a'])
+print("Instance is now running")
